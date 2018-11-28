02e54ec0a4f12331b17d888716a7026ec7dc3b4b
p.leroux <pleroux@student.42.fr>
2018-11-28 17:34:20 +0100

    up ng

--- angular-restaurant-app/src/app/app.module.ts
+++ angular-restaurant-app/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './items/items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular-restaurant-app/src/app/items/items.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Item } from './item.interface';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css']
+})
+export class ItemsComponent implements OnInit {
+  items: Item [] = [{
+    name: 'Pizza',
+    price: 3
+  },
+  {
+    name: 'Salad',
+    price: 2
+  }];
+  itemSubmitted = false;
+  itemForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  addToCart() {
+    window.alert('Added');
+  }
+
+  ngOnInit() {
+    // Initiating the form with the fields and the required validators
+    this.itemForm = this.formBuilder.group({
+      name: ['', Validators.required], // Name is required
+      price: ['', [Validators.required, Validators.min(0)]] // Price is required and must be a positive number
+    });
+  }
+
+  get getItemForm() {
+    return this.itemForm.controls;
+  }
+
+  addNewItem() {
+    this.itemSubmitted = true;
+    if (this.itemForm.invalid) {
+      console.log(this.itemForm);
+    } else {
+      this.items.push(this.itemForm.value);
+    }
+  }
+}
